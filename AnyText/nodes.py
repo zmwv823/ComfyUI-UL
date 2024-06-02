464154de55f85e9c38cfab48366ff69d10031a80
zmwv823 <913720128@qq.com>
2024-06-02 16:02:05 +0800

    Add chinese README for AnyText,为AnyText增加中文README.

--- AnyText/nodes.py
+++ AnyText/nodes.py
@@ -196,9 +196,9 @@ class AnyText:
         print("\033[93mBackend scripts location(后端脚本位置):\033[0m", path)
         pipe = pipeline('my-anytext-task', model=path, use_fp16=True, use_translator=False)
         n_lines = count_lines(prompt)
-        print("\033[93mNumber of text-content to draw(需要生成的文本数量):\033[0m", n_lines)
+        print("\033[93mNumber of text-content to generate(需要生成的文本数量):\033[0m", n_lines)
         ref_image = ori_image
-        print("\033[93mpos_imagg location(遮罩图位置):\033[0m", pos_image)
+        print("\033[93mpos_image location(遮罩图位置):\033[0m", pos_image)
         print("\033[93mori_image location(原图位置):\033[0m", ori_image)
         if Random_Gen == True:
             pos_img = generate_rectangles(width, height, n_lines, max_trys=500)
@@ -248,8 +248,8 @@ class AnyText:
                 "seed": seed,
                 "draw_pos": pos_img,
                 }
-        print("\033[93mDraw Order(文本生成优先位置):\033[0m", sort_radio)
-        print("\033[93mrevise_pos set to(位置修正设置):\033[0m", revise_pos)
+        print("\033[93mSort Position(文本生成位置排序):\033[0m", sort_radio)
+        print("\033[93mEnable revise_pos(启用位置修正):\033[0m", revise_pos)
         x_samples, results, rtn_code, rtn_warning, debug_info = pipe(input_data, **params)
         if rtn_code < 0:
             raise Exception(f"Error in AnyText pipeline: {rtn_warning}")
@@ -333,7 +333,7 @@ class AnyText_Pose_IMG:
         inverted_mask_image = invert_mask.reshape((-1, 1, mask.shape[-2], mask.shape[-1])).movedim(1, -1).expand(-1, -1, -1, 3)
         i = 255. * inverted_mask_image.cpu().numpy()[0]
         img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
-        print("\033[93mInput img Resolution(输入图像分辨率):\033[0m", width, "x", height)
+        print("\033[93mInput img Resolution,Max 768x768 Recmmended(输入图像分辨率,最大建议768x768):\033[0m", width, "x", height)
         img.save("custom_nodes\ComfyUI-UL\AnyText\comfy_mask_pos_img.png")
 
         return (
