070dde05dc33db6875a56a7122ea1c4c0579e4ea
zmwv823 <913720128@qq.com>
2024-06-09 19:57:19 +0800

    MiaoBi Nodes Update
    Former-commit-id: c76fb8a576a9938daa54c2701c1f8d69b8017b1f

--- MiaoBi/utils.py
+++ MiaoBi/utils.py
@@ -1,4 +1,3 @@
-import os
 import folder_paths
 
 class MiaoBi_Loader:
@@ -7,13 +6,9 @@ class MiaoBi_Loader:
         file_list = folder_paths.get_filename_list("loras")
         return {
             "required": {
-                "ckpt_name": (folder_paths.get_filename_list("checkpoints"),),
-                #os.listdir列出clip文件夹下所有文件及文件夹，输出选择的项目名字非路径，类型为字符串string，使用utf-8开启非英文支持。
-                "clip":(os.listdir(os.path.join(folder_paths.models_dir, "clip")), 'utf-8', ),
-                #从configs获取所有文件,默认输出字符串值v1-inference_fp16.yaml，定义默认加载哪个yaml文件。
-                "config_name": (folder_paths.get_filename_list("configs"),  {"default": "v1-inference_fp16.yaml"}),
+                "Apply_Hidiffusion": ("BOOLEAN", {"default": False},),
+                "clip_layer":("INT", {"default": -1, "min": -9999, "max": 9999}),
                 "LoRA_name1": (file_list, ),
-                #获取列表值，如果140行启用将添加None到第一位，可以用于开启判定
                 "LoRA_switch1": ("BOOLEAN", {"default": False},),
                 "lora_weight1": ("FLOAT", {
                     "default": 1,
@@ -21,7 +16,7 @@ class MiaoBi_Loader:
                     "max": 100, 
                     "step": 0.01
                 }),
-                "LoRA_name2": (file_list, ),#可以添加None判定
+                "LoRA_name2": (file_list, ),
                 "LoRA_switch2": ("BOOLEAN", {"default": False},),
                 "lora_weight2": ("FLOAT", {
                     "default": 1,
@@ -29,7 +24,7 @@ class MiaoBi_Loader:
                     "max": 100, 
                     "step": 0.01
                 }),
-                "LoRA_name3": (file_list, ),#可以添加None判定
+                "LoRA_name3": (file_list, ),
                 "LoRA_switch3": ("BOOLEAN", {"default": False},),
                 "lora_weight3": ("FLOAT", {
                     "default": 1,
@@ -43,9 +38,6 @@ class MiaoBi_Loader:
                     "max": 9999999,
                     "step": 0.01
                 }),
-                # "diffusion_name": (os.listdir(os.path.join(folder_paths.models_dir, "diffusion")), 'utf-8', ),
-                # os.listdir列出diffusers文件夹下的所有文件（包括文件夹），使用utf-8开启非英文支持，输出选择的文件夹或者文件的名字，类型字符串string值。
-                "diffusion_name": (["ShineChen1024/MiaoBi","SG161222/Realistic_Vision_V6.0_B1_noVAE","Lykon/dreamshaper-8","emilianJR/epiCRealism","Yntec/epiCPhotoGasm","runwayml/stable-diffusion-v1-5","Niggendar/counterfeitv30_fix_fp16","stablediffusionapi/rev-animated","sam749/CyberRealistic-v4-2","Lykon/dreamshaper-8-lcm"], {"default": "ShineChen1024/MiaoBi"}),
                 }
         }
          
@@ -56,22 +48,20 @@ class MiaoBi_Loader:
     TITLE = "MiaoBi Loader"
     
     def MiaoBi_Loader(self,
-                    ckpt_name, #0
-                    clip, #1
-                    config_name, #2
-                    LoRA_name1, #3
-                    LoRA_name2, #4
-                    LoRA_name3, #5
-                    lora_weight1, #6
-                    lora_weight2, #7
-                    lora_weight3, #8
-                    LoRA_switch1, #9
-                    LoRA_switch2, #10
-                    LoRA_switch3, #11
-                    multi_loras_weight, #12
-                    diffusion_name, #13
+                    Apply_Hidiffusion, #0
+                    clip_layer, #1
+                    LoRA_name1, #2
+                    LoRA_name2, #3
+                    LoRA_name3, #4
+                    lora_weight1, #5
+                    lora_weight2, #6
+                    lora_weight3, #7
+                    LoRA_switch1, #8
+                    LoRA_switch2, #9
+                    LoRA_switch3, #10
+                    multi_loras_weight, #11
                     ):
-        self.MiaoBi_paths = ckpt_name + '|' + clip + '|' + config_name + '|' + LoRA_name1 + '|' + LoRA_name2 + '|' + LoRA_name3 + '|' + str(lora_weight1) + '|' + str(lora_weight2) + '|' + str(lora_weight3) + '|' + str(LoRA_switch1) + '|' + str(LoRA_switch2) + '|' + str(LoRA_switch3) + '|' + str(multi_loras_weight) + '|' + diffusion_name
+        self.MiaoBi_paths = str(Apply_Hidiffusion) + '|' + str(clip_layer) + '|' + LoRA_name1 + '|' + LoRA_name2 + '|' + LoRA_name3 + '|' + str(lora_weight1) + '|' + str(lora_weight2) + '|' + str(lora_weight3) + '|' + str(LoRA_switch1) + '|' + str(LoRA_switch2) + '|' + str(LoRA_switch3) + '|' + str(multi_loras_weight)
         print(self.MiaoBi_paths)
         return (self.MiaoBi_paths, self.MiaoBi_paths,)
     
